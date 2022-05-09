7182409f875da520b9e23c57119cff8b34883974
midoritiba <mic.tiba@gmail.com>
2022-05-09 18:07:37 +0200

    Search event component

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,23 @@
-const Events = () => {
+import { Fragment } from 'react'
+import { useRouter } from 'next/router'
+import { getAllEvents } from '../../data'
+import EventList from '../../components/events/event-list'
+import EventSearch from '../../components/events/event-search'
+
+const AllEvents = () => {
+    const events = getAllEvents()
+    const router= useRouter()
+    
+    const findEventsHandler = (year, month) => {
+        const fullPath = `/events/${year}/${month}`
+        router.push(fullPath)
+    }
     return (
-        <div>
-            <h1>Event Home</h1>
-        </div>
+        <Fragment>
+            <EventSearch onSearch={findEventsHandler} />
+            <EventList events={events} />
+        </Fragment>
     );
 }
  
-export default Events;
+export default AllEvents;
